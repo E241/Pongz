2d892125a3be4433652886b4e6662cd3cda57725
Nibor9198 <robin.boregrim@gmail.com>
2016-01-27 13:40:04 +0100

    Initial Commit
    Bluh.

---
+++ desktop/src/se/doverfelt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.doverfelt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.doverfelt.PongzStart;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PongzStart(), config);
+	}
+}
---
+++ html/src/se/doverfelt/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package se.doverfelt.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import se.doverfelt.PongzStart;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PongzStart();
+        }
+}
