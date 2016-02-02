071668b1876b2de1951523ccd99a0ab23ca0536b
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2016-02-02 19:09:10 +0100

    Began work on sounds

--- core/src/se/doverfelt/entities/EntityPaddle.java
+++ core/src/se/doverfelt/entities/EntityPaddle.java
@@ -2,6 +2,7 @@ package se.doverfelt.entities;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -22,6 +23,7 @@ public class EntityPaddle implements Collidable {
     private float x,y , width = 2f, height = 15, yv = 0.2f;
     boolean isRight;
     private Rectangle bounds;
+    private Sound bounce;
 
     public EntityPaddle(float xIn,float yIn, boolean Right){
         x = xIn;
@@ -31,6 +33,7 @@ public class EntityPaddle implements Collidable {
         img = new Sprite(new Texture("white.png"));
         img.setPosition(x, y);
         img.setSize(width, height);
+        bounce = Gdx.audio.newSound(Gdx.files.internal("bounce.wav"));
         bounds = new Rectangle(x, y, width, height);
     }
     
@@ -69,6 +72,8 @@ public class EntityPaddle implements Collidable {
 
     @Override
     public void collide(Entity other) {
-
+        if (other instanceof EntityBall) {
+            bounce.play();
+        }
     }
 }
